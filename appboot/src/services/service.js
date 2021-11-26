5b94fc32d944843df930e70b3b2c01f53cd671a6
Paulo R C Trindade <Paulinhotrindade121212@gmail.com>
2021-11-26 15:34:22 -0300

    Update service.js

--- appboot/src/services/service.js
+++ appboot/src/services/service.js
@@ -4,6 +4,8 @@ const BASE_URL = 'https://chat.utalk.chat/login/sb8cdn3';
 
 const mensage = 'Olá !, O Nosso Horário de atendimento via whatsApp é somente de *Segunda a Sexta  das 08:00h as 18:00h*. Obrigado! '
 
+const mensageBoasVindas = 'Olá seja bem vindo ao atendimento da América Soft, aguarde um instante que um dos nossos colaboradores realizará o seu atendimentto '
+
 const login = 'autoatendimento';
 
 const senha = '12345';
@@ -49,79 +51,34 @@ const whats = {
 
             async function loadNewMensage(page, selector){
 
-                await whats.page.waitFor(3000)
-
-                console.log('entrou na func')
-
-                console.log(selector);
-
                 const newMensage = await page.$(selector);
-
-                
+								
+								await whats.page.waitFor(4000)
 
                  if (newMensage){
 
-                    await whats.page.waitFor(2000)
+                    console.log('Mensagem em atendimento');
 
-                    console.log('entrou no IF');
+										await newMensage.click();
 
-                    const flat = await whats.page.$('[class="friend-text"]');
+										await whats.page.waitFor(3000)
 
+										const inputMensagemAutomatica = await whats.page.$('[class="input-message inputChat"]');
 
+										await whats.page.waitFor(5000)
 
-                    if(flat){
+										await inputMensagemAutomatica.type(mensageBoasVindas);
+										
+										const sendMensagem = await whats.page.$('[class="send-button"]')
 
-                        await whats.page.waitFor(10000);
-
-                        console.log('entrou no flat');
-
-                        const newat = await whats.page.$('[class="fa fa-mobile-alt"]')
-
-                        await whats.page.waitFor(10000);
-
-                        let teste = [];
-
-
-
-                        //teste = document.querySelectorAll('div.friend-template.list');
-                        console.log('-------')
-                        console.log(document.querySelectorAll('div.friend-template.list'));
-
-                        if(newat){
-
-
-
-                           // await whats.page.click('[class="fa fa-mobile-alt"]');
-
-                        }
-
-                        
-
-                    }
-
+										 await sendMensagem.click();
                     
-
-                    // newMensage.click();
-
-                    // await whats.page.waitFor(3000);
-    
-                    // await whats.page.type('[class="_2vbn4"]',mensage);
-
-                    // await whats.page.waitFor(2000);
-    
-                    // await whats.page.click('[data-icon="send"]');
-
-                    // await loadNewMensage(page,selector);
-
-                    // //const toFile = await whats.page.$('[class="_3m_Xw"]');
-
-                    // await whats.page.waitFor(1000)
-            
-                    
-                }
+                }else {
+									console.log('Nenhuma mensagemm em atendimento ')    
+								}
             }
 
-            await loadNewMensage(whats.page, '[class="friend-operador"');
+            await loadNewMensage(whats.page, '[class="fa fa-mobile-alt"');
             
     },
 
@@ -137,7 +94,7 @@ const whats = {
 
                 await whats.page.waitFor(1000);
 
-                await whats.page.type('[class="_2vbn4"]',mensage);
+                await whats.page.type('[class="_2vbn4"]',mensageBoasVindas);
 
                 await whats.page.waitFor(2000);
 
