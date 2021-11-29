33bf859148e0534f40c80dd8fc2b08b1c971b739
Paulo R C Trindade <Paulinhotrindade121212@gmail.com>
2021-11-29 13:09:05 -0300

    Update index.js

--- appboot/src/services/index.js
+++ appboot/src/services/index.js
@@ -1,18 +1,24 @@
 const whats = require('./service');
 
+const diaSemana = new Date().getDay();
+const hora = new Date().getHours();
+const feriado = false;
 
-(async ()=>{
+(async () => {
 
+	await whats.initialize();
 
-    await whats.initialize();
-
-    while (true){
-
-      
-      await whats.mensage();
-
-      //await whats.toFile();
-    }
-
+	while (true) {
+		if (diaSemana !== 0 && diaSemana !== 6 && feriado === false) { // Dias úteis
+			if (hora >= 8 && hora <= 17) { // Dia útil dentro do horário
+				console.log('chamda da função')
+				await whats.mensage();
+			} else { // Dia útil fora do horário
+				await whats.toFile();
+			}
+		} else {
+			await whats.toFile(); // Final de semana ou Feriado 
+		}
+	}
 
 })();
